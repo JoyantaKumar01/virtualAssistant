4e6cabd0fb537814584a0300979fcad71795e9e3
Joyanta Kumar Das <jpjoyantakumardas01809@gmail.com>
2026-04-15 18:51:11 +0600

    modify backend folder

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,25 @@
 import express from "express"
 import dotenv from "dotenv"
 dotenv.config()
+import connectDb from "./config/db.js"
+import authRouter from "./routes/auth.routes.js"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
 
 const app=express()
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 const port=process.env.PORT || 5000
+app.use(express.json())
+app.use(cookieParser())
+app.use("/api/auth",authRouter)
 
 
 
 app.listen(port,()=>{
+    connectDb()
     console.log("server started")
 })
